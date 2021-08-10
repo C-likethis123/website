975843c9e61479caaa0770de89b578ea8282bbcb
Chow Jia Ying <chowjiaying211@gmail.com>
2021-08-10 10:54:57 +0800

    fix: data not being shown in the website

---
+++ data/jobs.js
@@ -0,0 +1,41 @@
+const jobs = [
+  {
+    company: "Shopee",
+    position: "Software Engineering Intern",
+    duration: "May 2021 - Aug 2021",
+    tasks: [
+      "Implemented features for an internal portal that manages banners used in Shopee's ecommerce campaigns."
+    ]
+  },
+  {
+    company: "Switcheo",
+    position: "Frontend Developer Intern",
+    duration: "July 2020 - Dec 2020",
+    tasks: [
+      "Worked on DEMEX, a decentralised exchange for margin and derivatives trading, using React, Typescript and CSS",
+      "Implemented responsive web designs with light/dark theming in partnership with UI/UX designers",
+      "Participated in usability tests and identified bugs, ensuring that bugs were caught and fixed in a timely manner",
+      "Configured TSLint rules in codebases, saving developers' time in linting their code",
+    ]
+  },
+  {
+    company: "Ecquaria Technologies",
+    position: "System Support Intern",
+    duration: "May 2019 - Aug 2019",
+    tasks: [
+      "Joined a team maintaining SkillsFuture Singapore's (SSG) backend management system",
+      "Investigated bug reports filed by users by checking logs, databases and testing the website",
+      "Wrote SQL queries to extract data from databases for SSG's data analytics team",
+      "Performed deployment and regression testing after each deployment"
+    ]
+  },
+  {
+    company: "DSO National Laboratories",
+    position: "Information Division Intern",
+    duration: "Jan 2018 - April 2018",
+    tasks: [
+      "Developed a proof of concept geofencing program using the Open Source Trusted Execution Environment Framework (OP-TEE) that runs on a Raspberry Pi",
+      "The OP-TEE framework aims to provide an isolated environment to run programs that require a more secure environment than normal applications."
+    ]
+  }
+]
---
+++ data/projects.js
@@ -0,0 +1,58 @@
+const projects = [
+  {
+    type: "Desktop Apps",
+    projects: [{
+      name: "Duke Chatbot",
+      description: "A personal chatbot application that helps users keep track of tasks, written in Java",
+      github: "https://github.com/C-likethis123/duke/releases",
+      image: "duke.png"
+    },
+    {
+      name: "ORGANice",
+      description: "An organ matching desktop application that helps hospitals find potential organ matches, written in Java",
+      github: "https://github.com/AY1920S1-CS2103T-T13-1/main/releases",
+      image: "organice.png"
+    }]
+  },
+  {
+    type: "Websites",
+    projects: [{
+      name: "To Do List App",
+      description: "A To Do List Web App written in Ruby on Rails, React and Redux",
+      demo: "http://tranquil-cliffs-23183.herokuapp.com/",
+      github: "https://github.com/C-likethis123/cvwo-assignment-2019",
+      image: "todolist.png",
+    },
+    {
+      name: "Blogger Alternative",
+      description: "A blog allowing users to post to their Google blogs, with Markdown support",
+      github: "https://github.com/C-likethis123/Blogger-Alternative",
+      image: "blogger-alt.png",
+    },
+    {
+      name: "Singapore Stock Screener",
+      description: "A website that screens for potential Singaporean stocks to invest based on investing philosophies",
+      github: "https://github.com/kangyeelim/AngelHack",
+      image: "stock-screener.png",
+    }]
+  },
+  {
+    type: "Open Source Contributions",
+    projects: [
+      {
+        name: "TUI Font Size Picker",
+        description: "A TUI Editor plugin that helps users edit their font sizes. Supports Markdown and WYSIWYG modes.",
+        github: "https://github.com/C-likethis123/tui-font-size-picker",
+        image: "font-size-plugin-demo.gif",
+      }]
+  },
+  {
+    type: "Programming Language Endeavours",
+    projects: [{
+      name: "Type Inferencer in Source 1",
+      description: "A type inferencer that inferences the types of constants, conditionals and functions for Source 1, a dialect of Javascript created for teaching purposes.",
+      github: "https://github.com/source-academy/js-slang/pull/610",
+      image: "type-inferencer.png",
+    }]
+  }
+]
